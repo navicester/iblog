6f07895062ba7ae031de404aa32c38579f75d7b7
navicester <navicester@qq.com>
2018-04-16 23:54:51 +0800

    08 Customize Admin

--- posts/models.py
+++ posts/models.py
@@ -8,7 +8,7 @@ class Post(models.Model):
     title = models.CharField(max_length=120)
     content = models.TextField()    
     updated = models.DateTimeField(auto_now=False, auto_now_add=True)
-    timestap = models.DateTimeField(auto_now=True, auto_now_add=False)
+    timestamp = models.DateTimeField(auto_now=True, auto_now_add=False)
 
     def  __unicode__(self):
         return self.title
