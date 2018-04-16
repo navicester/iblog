d33f0d421594f8ae813f77440ecf6b7d71785f05
navicester <navicester@qq.com>
2018-04-16 23:29:46 +0800

    06 First APP & Model

---
+++ posts/models.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=120)
+    content = models.TextField()    
+    updated = models.DateTimeField(auto_now=False, auto_now_add=True)
+    timestap = models.DateTimeField(auto_now=True, auto_now_add=False)
+
+    def  __unicode__(self):
+        return self.title
+
+    # # python3
+    def __str__(self):
+        return self.title
