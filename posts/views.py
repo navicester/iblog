ae9fae19c0d2770df7df99f6537255a81db347a0
navicester <navicester@qq.com>
2018-04-17 22:26:24 +0800

    16 QuerySet Basic

--- posts/views.py
+++ posts/views.py
@@ -4,6 +4,8 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Post
+
 # Create your views here.
 def post_create(request):
     return HttpResponse("<h1>Create</h1>")
@@ -23,13 +25,20 @@ def post_delete(request):
 
 def post_list(request):
     # return HttpResponse("<h1>List</h1>")   
-    if request.user.is_authenticated()  :
-        context = {
-            "title" : "my user list"
-        }
-    else:
-        context = {
-            "title" : "list"
-        }
+
+    # if request.user.is_authenticated()  :
+    #     context = {
+    #         "title" : "my user list"
+    #     }
+    # else:
+    #     context = {
+    #         "title" : "list"
+    #     }
+
+    queryset = Post.objects.all()
+    context = {
+        'object_list': queryset,
+        'title': 'List',
+    }
 
     return render(request, "index.html",context)      
