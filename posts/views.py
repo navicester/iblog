64606745d5b333a5a46cc28f0056eb22a04004d8
navicester <navicester@qq.com>
2018-04-17 22:58:34 +0800

    17 Get Item or 404 Query

--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
 from .models import Post
@@ -12,10 +12,12 @@ def post_create(request):
 
 def post_detail(request):
     # return HttpResponse("<h1>Detail</h1>")
+    instance = get_object_or_404(Post, title="title")
     context = {
-        "title" : "detail"
+        "title" : instance.title,
+        "instance" : instance
     }
-    return render(request, "index.html",context)   
+    return render(request, "post_detail.html",context)   
 
 def post_update(request):
     return HttpResponse("<h1>Update</h1>")
