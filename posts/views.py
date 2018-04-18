46fb8b2caa404ce60bd50ca4df2083ba45f92c99
navicester <navicester@qq.com>
2018-04-18 08:45:07 +0800

    20 ModelForm & Create View

--- posts/views.py
+++ posts/views.py
@@ -5,10 +5,21 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
 from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 def post_create(request):
-    return HttpResponse("<h1>Create</h1>")
+    # return HttpResponse("<h1>Create</h1>")
+    form = PostForm(request.POST or None)
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.save()
+
+    context = {
+        'form':form
+    }
+
+    return render(request, 'post_create.html', context)
 
 def post_detail(request, id=None):
     # return HttpResponse("<h1>Detail</h1>")
