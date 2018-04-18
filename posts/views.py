af53fed6b80607d340ec472c075360824c36c9c7
navicester <navicester@qq.com>
2018-04-18 21:46:06 +0800

    37 Search

--- posts/views.py
+++ posts/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.contrib import messages 
 from django.utils import timezone
+from django.db.models  import Q
 from urllib import quote_plus
 
 from .models import Post
@@ -101,6 +102,15 @@ def post_list(request):
     queryset_list = Post.objects.active()
     if request.user.is_staff or request.user.is_superuser:
         queryset_list = Post.objects.all()
+
+    query = request.GET.get("q")
+    if query:
+        queryset_list = queryset_list.filter(
+            Q(title__icontains=query) |
+            Q(content__icontains=query) | 
+            Q(user__first_name__icontains=query) |
+            Q(user__last_name__icontains=query)
+            ).distinct()
 
     paginator = Paginator(queryset_list, 2)
 
