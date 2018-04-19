2e26afc1f7c79be4ba809ee8616af1388d079da8
navicester <navicester@qq.com>
2018-04-19 22:24:04 +0800

    04 Implement Django Pagedown for Stack Overflow style Markdown

--- posts/forms.py
+++ posts/forms.py
@@ -1,9 +1,11 @@
 
 from django import forms
-
+from pagedown.widgets import PagedownWidget
 from .models import Post
 
 class PostForm(forms.ModelForm):
+    publish = forms.DateField(widget=forms.SelectDateWidget)
+    content = forms.CharField(widget=PagedownWidget)
     class Meta:
         model = Post
         fields = [
